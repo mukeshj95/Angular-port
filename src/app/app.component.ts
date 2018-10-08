3a5bedef166a5eb7c9483d8566e74731cce50751
Mukesh hp <mukeshj95@gmail.com>
2018-10-08 19:36:31 +0530

    added servers component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,11 +18,4 @@ import { FormsModule } from '@angular/forms';
   bootstrap: [AppComponent]
 })
 export class AppComponent {
-  tiles = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
-  typesOfShoes = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,10 +21,14 @@ import {MatExpansionModule} from '@angular/material/expansion';
 import {MatListModule} from '@angular/material/list';
 import {LayoutModule} from '@angular/cdk/layout';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ServerComponent,
+    ServersComponent,
   ],
   imports: [
     MatIconModule,
